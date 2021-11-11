5cc0661d66e31d75f75e23e4911aa00bcfb03368
Gorank Kansal <70534075+g1234or5@users.noreply.github.com>
2021-11-11 16:13:18 +0530

    add files via upload

--- Week 3/Duplicates in unsorted array/duplicate in unsortedarray.cpp
+++ Week 3/Duplicates in unsorted array/duplicate in unsortedarray.cpp
@@ -1,1 +1,54 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int partition(int arr[], int st, int en){
+    int pivot = arr[en];
+    int pos=st, i;
+    for(i=st; i<en; ++i){
+        if(arr[i]<pivot){
+            swap(&arr[i], &arr[pos++]);
+        }
+    }
+    swap(&arr[pos], &arr[en]);
+    return pos;
+}
+void quicksort(int arr[], int st, int en){
+    if(st<en){
+        int mid = partition(arr, st, en);
+        quicksort(arr, st, mid-1);
+        quicksort(arr, mid+1, en);
+    }
+}
+bool searchDuplicates(int arr[], int st, int en){
+    quicksort(arr, st, en);
+    int i;
+    for(i=0; i<en; ++i){
+        if(arr[i]==arr[i+1]){
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    int t, n, i;
+    cin >> t;
+    while(t){
+        cin >> n;
+        int* arr = new int[n];
+        for(i=0; i<n; ++i){
+            cin >> arr[i];
+        }
+        bool x = searchDuplicates(arr, 0, n-1);
+        if(x) 
+            cout << "YES\n";
+        else 
+            cout << "NO\n";
+        free(arr);
+        --t;
+    }
+    return 0;
+}

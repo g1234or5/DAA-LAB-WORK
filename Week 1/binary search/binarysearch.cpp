b6149ed3ee7b2dadc24957f4627d8ffc3574e0fe
Gorank Kansal <70534075+g1234or5@users.noreply.github.com>
2021-11-11 15:42:11 +0530

    add files via upload

--- Week 1/binary search/binarysearch.cpp
+++ Week 1/binary search/binarysearch.cpp
@@ -1,1 +1,36 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10]={10,20,30,40,50,60,70,80,90,100};
+    int n,left=0,right=9,mid,res=0;
+    int com=0;
+    cin>>n;
+    while(left<=right)
+    {
+        mid=(left+right)/2;
+        com++;
+        if(arr[mid]==n)
+        {
+            res=mid;
+            break;
+        }
+        else if(arr[mid]<n)
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right=mid-1;
+        }
+    }
+    if(res==0)
+    {
+        cout<<"Not Found!"<<endl;
+    }
+    else
+    {
+        cout<<"The element is at "<<res<<"th index"<<endl;
+        cout<<com<<" elements are compared.";
+    }
+}

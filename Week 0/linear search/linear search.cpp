357a1c957d1a31f37a5e754889ba3e339f363c55
Gorank Kansal <70534075+g1234or5@users.noreply.github.com>
2021-11-11 17:07:47 +0530

    add files via uploaf

--- Week 0/linear search/linear search.cpp
+++ Week 0/linear search/linear search.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10]={10,20,30,40,50,60,70,80,90,100};
+    int n,i,res=0;
+    cin>>n;
+    int x;
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]==n)
+        {
+            res=i;
+            x=i+1;
+        }
+    }
+    
+    if(res==0)
+    {
+        cout<<"Not Found!"<<endl;
+    }
+    else
+    {
+         cout<<"The element is at "<<res<<"th index."<<endl;
+         cout<<x<<" elements are compared.";
+    }
+}
